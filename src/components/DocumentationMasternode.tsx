bd455f1942e49faef05deb69e08cce6cf5e182b6
JOBOYA <lena.boya26@gmail.com>
2023-08-28 09:30:50 +0200

    guide

--- src/components/DocumentationMasternode.tsx
+++ src/components/DocumentationMasternode.tsx
@@ -1,153 +1,93 @@
-import React from "react";
-import { Link } from "react-scroll";
-import { DocumentationContent }  from './DocumentationContent';
+import React, { ReactNode, useState } from 'react';
+
+interface Section {
+  title: string;
+  content: ReactNode;
+}
+
+const sections: Section[] = [
+  {
+    title: 'Introduction',
+    content: (
+      <>
+        <p>Setting up a masternode on the Diabase Blockchain can be a rewarding way to contribute to the network's operations while earning rewards. This guide will walk you through the process of setting up your own Diabase Blockchain masternode, from preparing the necessary requirements to configuring and running your node.</p>
+        <br/>
+        <p><span style={{color: '#DAA520'}}>Note:</span> 
+        <br/>This guide assumes you have basic knowledge of Linux and command-line interfaces. If you're new to these concepts, consider seeking assistance or educating yourself further before proceeding.</p>
+<br/>
+        <p><span className='text-red-500 font-bold'>Requirements:</span></p>
+      
+        <ul>
+          <li>Technical Knowledge: Basic understanding of Linux commands and terminal usage.</li>
+          <li>Hardware: A dedicated server or Virtual Private Server (VPS) meeting the minimum requirements:</li>
+          <ul>
+            <li><span className='text-red-400'>CPU:</span> Multi-core processor (2 cores or more)</li>
+            <li><span className='text-slate-400'>RAM:</span> At least 2GB (more recommended)</li>
+            <li><span className='text-blue-500'>Storage:</span> 20GB of free disk space (SSD recommended)</li>
+            <li><span className='text-green-500'>Network:</span> Stable and high-speed internet connection</li>
+          </ul>
+          <li><span className='text-blue-500'>Diabase Blockchain (DIAC) Coins:</span> 1000 of Diabase Blockchain coins DIAC to use as collateral for your masternode.</li>
+          <li><span className='text-red-500'>Static IP Address:</span> Obtain a static IP address for your VPS or server.</li>
+        </ul>
+        <br/>
+        <p>This documentation describes the commands as if they were entered in the Diabase GUI by opening the console from Window {'>'} Console, but the same result can be achieved on a masternode by entering the same commands and adding the prefix ~/.diabasecore/diabase-cli to each command.</p>
+      </>
+    )
+  },
+  { title: 'Send the collateral', content:
+  (
+    <>
+      <p>A Diabase address with a single unspent transaction output <span className='text-blue-500'>(UTXO)</span>  of exactly <span className='text-blue-500'>1000 DIAC</span> is required to operate a masternode.</p>
+      
+      Once it has been sent, various keys regarding the transaction must be extracted for later entry in a 
+      <br/>configuration file and registration transaction as proof to write the configuration to the blockchain so the masternode can be included in the deterministic list.
+      
+      <br/>
+      <br/>A masternode can be registered from the official Diabase wallet or core cli.
+      
+    </>
+  ) },
+
+  { title: 'Guide d’installation', content: 'Ceci est le guide d’installation.' },
+];
+
+
+
+
+
+
+
 
 export const DocumentationMasternode: React.FC = () => {
+  const [selectedSection, setSelectedSection] = useState<Section>(sections[0]);
 
-
+  const handleSectionClick = (section: Section) => {
+    setSelectedSection(section);
+  };
 
   return (
-    <body className="bg-gray-50 dark:bg-slate-900 h-screen fixed w-full">
-
-  
-  <div className="sticky top-0 inset-x-0 z-20 bg-white border-y px-4 sm:px-6 md:px-8 lg:hidden dark:bg-gray-800 dark:border-gray-700">
-    <div className="flex items-center py-4">
-    
-      <button type="button" className="text-gray-500 hover:text-gray-600" data-hs-overlay="#application-sidebar-dark" aria-controls="application-sidebar-dark" aria-label="Toggle navigation">
-        <span className="sr-only">Toggle Navigation</span>
-        <span className="sr-only">Toggle Navigation</span>
-        <svg className="w-5 h-5" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
-        </svg>
-      </button>
-      
-
-     
-      <ol className="ml-3 flex items-center whitespace-nowrap min-w-0" aria-label="Breadcrumb">
-        <li className="flex items-center text-sm text-gray-800 dark:text-gray-400">
-          Application Layout
-         
-        </li>
-        <Link
-              activeClass="active"
-              to="section-1"
-              spy={true}
-              smooth={true}
-              offset={-70}
-              duration={500}
+    <div className="flex h-screen bg-opacity-40 bg-black backdrop-blur text-white">
+      <div className="w-1/4 bg-slate-800 p-4">
+        <ul>
+          {sections.map((section, index) => (
+            <li 
+              key={index}
+              className={`cursor-pointer p-2 hover:bg-gray-700 transition ease-in-out duration-150 ${selectedSection.title === section.title ? 'bg-gray-700' : ''}`}
+              onClick={() => handleSectionClick(section)}
             >
-             Introduction
-            </Link>
-      </ol>
-     
+              <span className="inline-block hover:px-4">{section.title}</span>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="flex-1 p-8 bg-slate-700 backdrop-blur-md ">
+        <h2 className="text-2xl font-semibold mb-4 text-green-400">{selectedSection.title}</h2>
+        <div 
+          className="w-full p-4 bg-gray-800 text-white border border-gray-700 rounded-2xl text-lg leading-relaxed"
+        >
+          {selectedSection.content}
+        </div>
+      </div>
     </div>
-  </div>
- 
-
- 
-  <div id="application-sidebar-dark" className="hs-overlay hs-overlay-open:translate-x-0 -translate-x-full transition-all duration-300 transform hidden fixed top-0 left-0 bottom-0 z-[60] w-64 bg-gray-900 border-r border-gray-800 pt-7 pb-10 overflow-y-auto scrollbar-y lg:block lg:translate-x-0 lg:right-auto lg:bottom-0">
-    <div className="px-6">
-      <a className="flex-none text-xl font-semibold text-white" href="#" aria-label="Brand">MASTERNODE SETUP</a>
-    </div>
-
-    <nav className="hs-accordion-group p-6 w-full flex flex-col flex-wrap" data-hs-accordion-always-open>
-      <ul className="space-y-1.5">
-        <li>
-          <a className="flex items-center gap-x-3 py-2 px-2.5 bg-gray-700 text-sm text-white rounded-md" href="javascript:;">
-           
-          Introduction
-          </a>
-        </li>
-
-        <li className="hs-accordion" id="users-accordion">
-          <a className="hs-accordion-toggle flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-           
-            Users
-
-           
-
-          </a>
-
-          <div id="users-accordion-child" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
-            <ul className="hs-accordion-group pl-3 pt-2" data-hs-accordion-always-open>
-              <li className="hs-accordion" id="users-accordion-sub-1">
-                <a className="hs-accordion-toggle flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                  Sub Menu 1
-
-                  
-                </a>
-
-                <div id="users-accordion-sub-1-child" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden">
-                  <ul className="pt-2 pl-2">
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 1
-                      </a>
-                    </li>
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 2
-                      </a>
-                    </li>
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 3
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-              <li className="hs-accordion" id="users-accordion-sub-2">
-                <a className="hs-accordion-toggle flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                  Sub Menu 2
-
-                  
-
-                </a>
-
-                <div id="users-accordion-sub-2-child" className="hs-accordion-content w-full overflow-hidden transition-[height] duration-300 hidden pl-2">
-                  <ul className="pt-2 pl-2">
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 1
-                      </a>
-                    </li>
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 2
-                      </a>
-                    </li>
-                    <li>
-                      <a className="flex items-center gap-x-3.5 py-2 px-2.5 text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-                        Link 3
-                      </a>
-                    </li>
-                  </ul>
-                </div>
-              </li>
-            </ul>
-          </div>
-        </li>
-
-        <li className="hs-accordion" id="account-accordion">
-          <a className="hs-accordion-toggle flex items-center gap-x-3.5 py-2 px-2.5 hs-accordion-active:text-white hs-accordion-active:hover:bg-transparent text-sm text-gray-400 rounded-md hover:bg-gray-800 hover:text-white" href="javascript:;">
-           
-            Account
-
-
-            
-          </a>
-
-          </li>
-      </ul>
-    </nav>
-  </div>
-
-
- <div className="mt-20 pl-[300px] fixed z-10">
-  <DocumentationContent />
-  </div>
- 
-</body>
   );
 };
